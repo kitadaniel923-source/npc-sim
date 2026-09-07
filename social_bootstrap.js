11f3a918e073410bcd7b7d99eec2524d21f675e7
kitadaniel923-source <kitadaniel923@gmail.com>
2026-09-07 21:06:07 +0200

    Load NPC visuals and world generation layers

--- social_bootstrap.js
+++ social_bootstrap.js
@@ -39,6 +39,8 @@
 
     await loadScript('social_combinations.js');
     await loadScript('npc_life_engine.js');
+    await loadScript('world_generation.js');
+    await loadScript('npc_visuals.js');
     await loadScript('social_ui.js');
   } catch (error) {
     console.error('Everglen bootstrap failed:', error);
