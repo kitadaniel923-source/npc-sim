f27b23aed47fd0be9d69109e62539a05c04e1e9e
kitadaniel923-source <kitadaniel923@gmail.com>
2026-09-08 03:43:48 +0200

    Archive duplicate interaction layer

---
+++ archive/interaction_fix.js
@@ -0,0 +1,3 @@
+Archived on 2026-09-08.
+
+This file previously rebound core simulation controls and duplicated input/render responsibilities already owned by simulation.js. The production build no longer loads it.
