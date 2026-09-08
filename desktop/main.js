487bd235395623bf745c8438511da82c1c24e71a
kitadaniel923-source <kitadaniel923@gmail.com>
2026-09-08 21:56:48 +0200

    Add Everglen Windows desktop launcher

---
+++ desktop/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const APP_ID = 'com.everglen.livingworld';
+app.setAppUserModelId(APP_ID);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1600,
+    height: 950,
+    minWidth: 1100,
+    minHeight: 700,
+    backgroundColor: '#111714',
+    show: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+      webSecurity: true,
+      spellcheck: false
+    }
+  });
+
+  win.once('ready-to-show', () => win.show());
+  win.loadFile(path.join(__dirname, '..', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  Menu.setApplicationMenu(null);
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
